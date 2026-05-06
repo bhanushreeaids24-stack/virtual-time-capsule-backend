5b734d13b617153ad2251e54477467885a0796c0
Bhanu Shree <bhanushree.aids24@cmrit.ac.in>
2026-05-06 23:00:24 +0530

    removed server folder from git

--- server/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateMessage } = require("../controllers/messageController");
-
-router.post("/generate", generateMessage);
-
-module.exports = router;
